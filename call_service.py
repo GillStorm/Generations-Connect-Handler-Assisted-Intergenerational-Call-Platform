3902681c5f2e645e027f4bbddeb3457243870291
Aarush Garg <aarushgarg2006@gmail.com>
2026-01-16 22:45:57 +0530

    Update call_service.py

--- call_service.py
+++ call_service.py
@@ -1,5 +1,6 @@
 from twilio.rest import Client
 from dotenv import load_dotenv
+from urllib.parse import quote
 import os
 
 load_dotenv()
@@ -13,11 +14,11 @@ TWILIO_NUMBER = os.getenv("TWILIO_PHONE_NUMBER")
 BASE_URL = os.getenv("BASE_URL")
 
 
-def call_handler(handler_phone, student_name, elder_name):
+def call_handler(handler_phone, student_name, student_phone, elder_name):
     return client.calls.create(
         to=handler_phone,
         from_=TWILIO_NUMBER,
-        url=f"{BASE_URL}/calls/handler-voice?student={student_name}&elder={elder_name}"
+        url=f"{BASE_URL}/calls/handler-voice?student={quote(student_name)}&student_phone={quote(student_phone)}&elder={quote(elder_name)}"
     )
 
 
@@ -25,5 +26,5 @@ def call_student(student_phone, conference_name):
     return client.calls.create(
         to=student_phone,
         from_=TWILIO_NUMBER,
-        url=f"{BASE_URL}/calls/student-voice?conf={conference_name}"
+        url=f"{BASE_URL}/calls/student-voice?conf={quote(conference_name)}"
     )
