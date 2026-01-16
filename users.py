97f687e612729e04e0e32f8d4641e5a58787c881
Aarush Garg <aarushgarg2006@gmail.com>
2026-01-16 22:51:03 +0530

    Update users.py

--- users.py
+++ users.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter
-from ..database import db
+from .. import database
 from ..models.schemas import User
 
 router = APIRouter()
 
 @router.get("/")
 def get_users():
-    users = [doc.to_dict() for doc in db.collection("users").stream()]
+    users = [doc.to_dict() for doc in database.db.collection("users").stream()]
     return {"users": users}
 
 @router.post("/")
 def add_user(user: User):
-    doc_ref = db.collection("users").add(user.dict())
+    doc_ref = database.db.collection("users").add(user.dict())
     return {"message": "User added successfully"}
