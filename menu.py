b6f6dd4749844c7a2f3917257eb10d2585b7a00c
Aarush Garg <aarushgarg2006@gmail.com>
2026-01-16 22:49:44 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter
-from ..database import db
+from .. import database
 from ..models.schemas import MenuItem
 
 router = APIRouter()
 
 @router.get("/")
 def get_menu():
-    menu_items = [doc.to_dict() for doc in db.collection("menu").stream()]
+    menu_items = [doc.to_dict() for doc in database.db.collection("menu").stream()]
     return {"menu": menu_items}
 
 @router.post("/")
 def add_menu_item(item: MenuItem):
-    db.collection("menu").add(item.dict())
+    database.db.collection("menu").add(item.dict())
     return {"message": "Menu item added successfully"}
