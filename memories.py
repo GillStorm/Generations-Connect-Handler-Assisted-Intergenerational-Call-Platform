a29343f9621971cc060be12d57ce8a25f3c4b316
Aarush Garg <aarushgarg2006@gmail.com>
2026-01-16 22:48:53 +0530

    Update memories.py

--- memories.py
+++ memories.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter
-from ..database import db
+from .. import database
 from ..models.schemas import Memory
 
 router = APIRouter()
 
 @router.get("/")
 def get_memories():
-    memories = [doc.to_dict() for doc in db.collection("memories").stream()]
+    memories = [doc.to_dict() for doc in database.db.collection("memories").stream()]
     return {"memories": memories}
 
 @router.post("/")
 def add_memory(memory: Memory):
-    db.collection("memories").add(memory.dict())
+    database.db.collection("memories").add(memory.dict())
     return {"message": "Memory added successfully"}
